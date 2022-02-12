88528ef801cf0df16ec3c05119fee5f0bc798e96
Orhan ARI <66368354+ariorhan@users.noreply.github.com>
2022-02-12 23:07:57 +0000

    Delete js1-day12.js

--- js1-day12.js
+++
@@ -1,9 +0,0 @@
- let greeting = document.querySelector("button")
- greeting.addEventListener("click", domClick)
-
- function domClick() {
-     alert("changed")
-     document.getElementById("greeting").innerHTML = "Hola WORLD"
-     // this.style.color == "red" ? this.style.color = "black" : this.style.color = "red"
- } 
-
